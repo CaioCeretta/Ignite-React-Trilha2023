9a374a76e572632bfb710be079e6a9224100caa2
andandtex <andandtex@gmail.com>
2024-06-29 22:03:20 -0300

    Finished the starting code

---
+++ 04-pizzashop-web/src/App.tsx
@@ -0,0 +1,13 @@
+import './globals.css'
+
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button variant={'secondary'}>Send</Button>
+    </div>
+  )
+}
+
+export default App
