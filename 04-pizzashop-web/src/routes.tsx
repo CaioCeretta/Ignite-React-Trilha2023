49f83892e957875c5ef166e127c76ef0777a0b3d
andandtex <andandtex@gmail.com>
2024-07-01 11:56:39 -0300

    Sign up page form

--- 04-pizzashop-web/src/routes.tsx
+++ 04-pizzashop-web/src/routes.tsx
@@ -4,6 +4,7 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/signin'
+import { SignUp } from './pages/auth/signup'
 
 export const router = createBrowserRouter([
   {
@@ -12,8 +13,11 @@ export const router = createBrowserRouter([
     children: [{ path: '/', element: <Dashboard /> }],
   },
   {
-    path: '/',
+    path: '/auth',
     element: <AuthLayout />,
-    children: [{ path: '/signin', element: <SignIn /> }],
+    children: [
+      { path: 'signin', element: <SignIn /> },
+      { path: 'signup', element: <SignUp /> },
+    ],
   },
 ])
