572b8813a931266cd446b76f48a02e2f82358170
andandtex <andandtex@gmail.com>
2024-07-15 14:14:17 -0300

    Set up Mock Service Worker

---
+++ 04-pizzashop-web/src/api/mocks/index.ts
@@ -0,0 +1,46 @@
+import { setupWorker } from 'msw/browser'
+
+import { env } from '@/env'
+
+/* MSW Explanation
+
+First we need to understand what are service workers. They are a type of web worker that run in the background, separate
+from the main browser thread and can control network requests, cache resources, and so on.
+
+In summary, they provide a powerful way to enhance a web application with offline capabilities, background synchronization
+and improved performance through caching. By running independently of the main browser thread, they enable features that
+contribute to a better user experience, even when network conditions are less than ideal
+
+________________________________________________________________________________________________________________________
+
+Mock Service Worker is a library used for mocking network requests in client-side and server-side applications.
+It intercepts HTTP requests and provides mock responses without the need of an actual server
+
+Some features include:
+
+- Intercepting Requests': MSW intercept the http requests made by the application, allowing us to control the responses
+and simulate different scenarios
+- Integration with popular testing libraries, like jest, cypress, and sorybook, making it easy to use in our existent test
+suites
+- Realistic Mocking, by providing data that resembles real API responses
+- Declarative API, making it easy to define request handler and responses
+
+
+
+
+*/
+
+export const worker = setupWorker()
+
+/*
+  By the moment we call this setupWorker, the mocks still are not going to function properly, the requests won't be intercepted
+  only when we clal the worker.start that we are defining in the function below, our mocks will take action. After it,
+  all the received requests will be intercepted by the mock service in our test environment
+*/
+export async function enableMSW() {
+  if (env.MODE !== 'test') {
+    return
+  }
+
+  await worker.start()
+}
--- 04-pizzashop-web/src/main.tsx
+++ 04-pizzashop-web/src/main.tsx
@@ -1,14 +1,13 @@
-import { QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { enableMSW } from './api/mocks/index.ts'
 import App from './app.tsx'
-import { queryClient } from './lib/react-query.ts'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+enableMSW().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
       <App />
-    </QueryClientProvider>
-  </React.StrictMode>,
-)
+    </React.StrictMode>,
+  )
+})
