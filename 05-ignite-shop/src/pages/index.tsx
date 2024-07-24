b10953b6cb999230de55691a93c7ae8d60dfb6b0
andandtex <andandtex@gmail.com>
2024-07-24 18:21:36 -0300

    Up to server side props and static site

--- 05-ignite-shop/src/pages/index.tsx
+++ 05-ignite-shop/src/pages/index.tsx
@@ -2,9 +2,9 @@
 
 import { styled } from "@/styles";
 import { HomeContainer, Product } from "@/styles/pages/home";
-import { useEffect, useRef, useState } from "react";
 import KeenSlider from "keen-slider";
 import "keen-slider/keen-slider.min.css";
+import { useEffect, useRef } from "react";
 
 const Button = styled('button', {
   backgroundColor: "$green-500",
@@ -21,14 +21,11 @@ const Button = styled('button', {
   }
 })
 
-import shirt1 from '@/assets/tshirt-1.png';
-import shirt2 from '@/assets/tshirt-2.png';
-import shirt3 from '@/assets/tshirt-3.png';
 
+import { stripe } from "@/lib/stripe";
+import { GetStaticProps } from "next";
 import { Roboto } from "next/font/google";
 import Image from "next/image";
-import { stripe } from "@/lib/stripe";
-import { GetServerSideProps } from "next";
 import Stripe from "stripe";
 
 const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
@@ -82,37 +79,73 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  /* If we do something like this, our whole page would take 2 seconds to load, and just after this data loaded, only then,
-  the page was available, and that is one thing important to know */
-  // await new Promise(resolve => setTimeout(resolve, 2000))
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   /* If we do something like this, our whole page would take 2 seconds to load, and just after this data loaded, only then,
+//   the page was available, and that is one thing important to know */
+//   // await new Promise(resolve => setTimeout(resolve, 2000))
 
-  // console.log('Runned')
+//   // console.log('Runned')
 
+//   const response = await stripe.products.list({
+//     /*When it is a list it comes from the property data inside of it, and if it was only one product we could expand directly
+//     the default price */
+//     expand: ['data.default_price']
+//   })
+
+//   /* Now we need to transform this response.data so we get only the data we want from it */
+
+
+
+//   const products = response.data.map((product) => {
+//     /* By doing this, i'm forcing the default price to be understood as a number with that type*/
+//     const price = product.default_price as Stripe.Price
+
+//     /* the price.unit_amount returns us the price, and it will come in cents, that is one good tip to always save prices
+//     as cents, because then we won't have problems with commas, float problems and that kind of thing, we just multiply
+//     it by 100, and when we are working with that number, we just divide it by 100
+//     */
+
+//     return {
+//       id: product.id,
+//       name: product.name,
+//       imageUrl: product.images[0],
+//       url: product.url,
+//       price: new Intl.NumberFormat('en-US', {
+//         style: 'currency',
+//         currency: 'USD'
+//       }).format(price.unit_amount! / 100)
+//     }
+//   })
+
+
+//   return {
+//     props: {
+//       products
+//     }
+//   }
+// }
+
+export const getStaticProps: GetStaticProps = async () => {
+  
+  /* The defaut price is used by stripe to "expand" certain attributes that, by default, are returned just as identifiers
+  or partial objcets. by using expand, we are requesting stripe api to include the complete details of the related resources
+  dyring the request.
+  
+  In our case, we are expanding the default_price of each product. Without expanding it, stripe would return only the price
+  id related to each product. WIth expand, stripe includes the complete price object inside the product, providing us details
+  such as the unit_amount, currency, etc. 
+  */
   const response = await stripe.products.list({
-    /*When it is a list it comes from the property data inside of it, and if it was only one product we could expand directly
-    the default price */
     expand: ['data.default_price']
   })
 
-  /* Now we need to transform this response.data so we get only the data we want from it */
-
-
-
-  const products = response.data.map((product) => {
-    /* By doing this, i'm forcing the default price to be understood as a number with that type*/
+  const products = response.data.map(product => {
     const price = product.default_price as Stripe.Price
-
-    /* the price.unit_amount returns us the price, and it will come in cents, that is one good tip to always save prices
-    as cents, because then we won't have problems with commas, float problems and that kind of thing, we just multiply
-    it by 100, and when we are working with that number, we just divide it by 100
-    */
 
     return {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      url: product.url,
       price: new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD'
@@ -120,10 +153,10 @@ export const getServerSideProps: GetServerSideProps = async () => {
     }
   })
 
-
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2 // 2 hours
   }
 }
