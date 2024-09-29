c6426d0d251eec3951713e4845d441bad9ed3aa7
Caio Ceretta <caioceretta@gmail.com>
2024-09-28 22:15:07 -0300

    doc: Added some explanation

--- 05-ignite-shop/src/styles/global.ts
+++ 05-ignite-shop/src/styles/global.ts
@@ -1,5 +1,17 @@
 import { globalCss } from ".";
 
+/* 
+  Here we use the globalCss function, which comes from our index.ts stitches initialization.
+
+  Here we will create a constant globalStyles which invokes that function, and passes global styling to our code.
+
+  Now, because we are going to load this style, only once in our app, it isn't recommended for us to place any css inside
+  the _document, so we'll place in the _app.tsx, which acts like a container for the pages in our app. It's like a component
+  that is loaded with every page in our app.
+
+  So every page is that <Component {...pageProps} /> we have inside the file
+*/
+
 export const globalStyles = globalCss({
   '*': {
     boxSizing: 'border-box',
