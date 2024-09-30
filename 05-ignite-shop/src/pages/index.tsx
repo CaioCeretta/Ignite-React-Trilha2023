c7053df941728bdd54e4d27b784f912e47a6a0bc
Caio Ceretta <caioceretta@gmail.com>
2024-09-30 00:05:14 -0300

    feat: Code and explanation to the home page

--- 05-ignite-shop/src/pages/index.tsx
+++ 05-ignite-shop/src/pages/index.tsx
@@ -1,163 +1,61 @@
-// File: /pages/home.js
-
-import { styled } from "@/styles";
-import { HomeContainer, Product } from "@/styles/pages/home";
-import KeenSlider from "keen-slider";
-import "keen-slider/keen-slider.min.css";
-import { useEffect, useRef } from "react";
-
-const Button = styled('button', {
-  backgroundColor: "$green-500",
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
 
 
-import { stripe } from "@/lib/stripe";
-import { GetStaticProps } from "next";
-import { Roboto } from "next/font/google";
-import Image from "next/image";
-import Stripe from "stripe";
-import Link from "next/link";
+import { HomeContainer, Product } from '@/styles/pages/home'
+import tshirt1 from '../assets/tshirt-1.png'
+import tshirt2 from '../assets/tshirt-2.png'
+import tshirt3 from '../assets/tshirt-3.png'
+import Image from 'next/image'
 
-const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
+/*
+  The styling comments will be present on the styles file  
 
-interface HomeProps {
-  products: {
-    id: string,
-    name: string,
-    imageUrl: string,
-    url: string,
-    price: number,
-  }[]
-}
+  First we are starting with static code, created styles for the HomeContainer
+  Then, we are creating a Product, which also is a styled div coming from the styles, it's a container for the product
 
-export default function Home({ products }: HomeProps) {
+  When using the next/image, we need to pass a width and a height for the image, it does not need to have the exact dimensions
+  the image will have, so let's say we put this number based on the bigger height and bigger width this image will
+  have. This way, next image, will, when it imports the image, it will resize this image so we don't import it too big.
+  If we don't tell the height and the width, and the image has way more than we are going to use, the whole image will be
+  imported.
 
-  const sliderRef = useRef(null);
-
-  useEffect(() => {
-
-    if (sliderRef.current) {
-      const slider = new KeenSlider(sliderRef.current, {
-        slides: { perView: 3, spacing: 48 },
-      });
+  Now, for the product details, we are going to use the footer, it normally is the best element for us to use in these
+  cases.
 
 
-      return () => {
-        slider.destroy();
-      };
-    }
-
-
-  }, []);
-
-  return (
-      <HomeContainer ref={sliderRef} className="keen-slider">
-        {products.map(product => (
-          <Link href={`/product/${product.id}`} key={product.id} prefetch={false} >
-          <Product
-            className="keen-slider__slide"
-          >
-            <Image src={product.imageUrl} width={520} height={400} alt={product.name} />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
-          </Link>
-        ))}
-      </HomeContainer>
-  );
-}
-
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   /* If we do something like this, our whole page would take 2 seconds to load, and just after this data loaded, only then,
-//   the page was available, and that is one thing important to know */
-//   // await new Promise(resolve => setTimeout(resolve, 2000))
-
-//   // console.log('Runned')
-
-//   const response = await stripe.products.list({
-//     /*When it is a list it comes from the property data inside of it, and if it was only one product we could expand directly
-//     the default price */
-//     expand: ['data.default_price']
-//   })
-
-//   /* Now we need to transform this response.data so we get only the data we want from it */
-
-
-
-//   const products = response.data.map((product) => {
-//     /* By doing this, i'm forcing the default price to be understood as a number with that type*/
-//     const price = product.default_price as Stripe.Price
-
-//     /* the price.unit_amount returns us the price, and it will come in cents, that is one good tip to always save prices
-//     as cents, because then we won't have problems with commas, float problems and that kind of thing, we just multiply
-//     it by 100, and when we are working with that number, we just divide it by 100
-//     */
-
-//     return {
-//       id: product.id,
-//       name: product.name,
-//       imageUrl: product.images[0],
-//       url: product.url,
-//       price: new Intl.NumberFormat('en-US', {
-//         style: 'currency',
-//         currency: 'USD'
-//       }).format(price.unit_amount! / 100)
-//     }
-//   })
-
-
-//   return {
-//     props: {
-//       products
-//     }
-//   }
-// }
-
-export const getStaticProps: GetStaticProps = async () => {
-
-  /* The defaut price is used by stripe to "expand" certain attributes that, by default, are returned just as identifiers
-  or partial objcets. by using expand, we are requesting stripe api to include the complete details of the related resources
-  dyring the request.
   
-  In our case, we are expanding the default_price of each product. Without expanding it, stripe would return only the price
-  id related to each product. WIth expand, stripe includes the complete price object inside the product, providing us details
-  such as the unit_amount, currency, etc. 
-  */
-  const response = await stripe.products.list({
-    expand: ['data.default_price']
-  })
 
-  const products = response.data.map(product => {
-    const price = product.default_price as Stripe.Price
+*/
 
-    return {
-      id: product.id,
-      name: product.name,
-      imageUrl: product.images[0],
-      price: new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD'
-      }).format(price.unit_amount! / 100)
-    }
-  })
+export default function Home() {
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={tshirt1}
+          alt="tshirt1"
+          width={520}
+          height={520}
+        />
 
-  return {
-    props: {
-      products
-    },
-    revalidate: 60 * 60 * 2 // 2 hours
-  }
+        <footer>
+          <strong>Shirt X</strong>
+          <span>$ 50.00</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={tshirt2}
+          alt="tshirt2"
+          width={520}
+          height={520}
+        />
+
+        <footer>
+          <strong>Shirt XI</strong>
+          <span>$ 50.00</span>
+        </footer>
+      </Product>
+
+
+    </HomeContainer>
+  )
 }
--- 05-ignite-shop/src/styles/pages/home.ts
+++ 05-ignite-shop/src/styles/pages/home.ts
@@ -1,36 +1,126 @@
+import image from "next/image";
 import { styled } from "..";
+
+/*
+  HomeContainer: styled main, which is of display flex, meaning the images will be one to the right of the other and will
+  be parent of the whole code, including the products
+
+  Product: styled div, it has a display of flex, contains the image and the footer, which is where we are going to display
+  the product informations
+
+  One thing we need to do here, is that we have the gap between the images, but we have a bigger margin on the left side.
+
+  And as we style, we may think: "Ok, i just need to add a margin left to the container" , but the problem here lies on
+  if we, for example, zoom out the page, because, for instance
+
+  We have a container of 1200px, our box is occupying 80% of it, that is the specific size plus the remaining margin it has
+  on the right, and this margin on the left, should also exist.
+
+  The is when we have to figure out what's going to happen if we zoom out the page. What should happen is, the left margin,
+  could increase but the box must remain "glued" to the right. 
+
+  So in pure css, it's not so simple to do this, what we'll need to do is:
+
+  1. The images container will have a fixed width, it will start showing two images and will eventually show more with the
+  carousel
+  2. We are going to need that the elements container have the width be 1180px + the additional margin we have to the right
+  3. To obtain the number we'll have to do a CSS calculation, which is 1180 + the right margin, and to get this number we
+  will take the screen width as a whole - 1180, that will return both the left and the right margin. Because now we have the
+  width of these 2 little boxes, we will divide this number by 2 so in the end it will be
+
+  1180 + ((screenWidth - 1180) / 2)
+
+  4. Now that we got this value, we are going to set it on the maxWidth of the container, set a marginLeft of auto, which
+  will cause the component to be pushed to the right, because now the left margin is all the remaining space
+
+  5. If we zoom out now, the div will keep growing to the right, because the left margin is the remaining space.
+
+
+  EXPLANATION OF THE INIFINITE BEHAVIOR
+
+  The reason this appears to be infinite to the right is mainly because the maxWidth calculation, and for these reasons.
+
+  1. 100vw is the width of the viewport, we're subtracting (100vw - 1180px) / 2, which ensures that the container is
+  centered and maintains a maximum width of 1180px within a viewport larget than that.
+
+  2. marginLeft: 'auto' pushes the container to the right, occupying all the remaining space on the left side. Since the
+  width is 100% of its parent container, due to width: '100%', and the maxWidth is restricting it only to a specific size,
+  the element appears to grow towards the right without boundaries beyond its maximum width.
+
+  So while the width is controlled by maxWidth, the margin-left makes it look like it expands indefinetly to the right within
+  the available space. In essence, the container isn't tryly infinite but sretches to fill the space until restricted by
+  the maxWidth rule.
+
+  The maxWidth in this case, is dynamic, because this calculation is responsive to the viewport width, so as the viewport
+  increases, the result of the calculation grows as well
+
+  ** END OF EXPLANATION **
+
+  Now, after we finally adjusted that behavior, for styling the product, which is a styled anchor
+  we'll start by adding a background linear gradient for each product
+
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)
+
+  0deg: Starts from the top and goes down
+  90 deg: Starts from the left and goes to the right
+  180deg: Starts from the bottom and goes to the top
+  270deg: starts from the right and goes to the left
+
+  
+
+
+*/
+
 
 export const HomeContainer = styled('main', {
   display: 'flex',
   width: '100%',
-  maxWidth: 'calc(100vw - ((100vw - 1180px) / 2 ) )',
+  gap: '3rem',
+  maxWidth: 'calc(100vw - (100vw - 1180px) / 2)',
   marginLeft: 'auto',
-  minHeight: 656
+  minHeight: '656px'
 })
 
-export const Product = styled('div', {
+export const Product = styled('a', {
   background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
   borderRadius: 8,
   cursor: 'pointer',
+  /* to indicate this is a link (if the link had an href the pointer woudln't be needed), */
+  padding: '0.25rem',
+  // Because the footer will have a position absolute, so it will be right at the bottom. over the image
   position: 'relative',
   overflow: "hidden",
 
+  /* For the image to be completely centralized in the container, it will be a flex, with both justify and align center. */
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
 
-  img: {
-    /* We imported that image with the dimensions of 520 x 440, one thing we can always do in this case is to always work
-    with the property objectFit: 'cover', it won't distort the image but it'll make it fit the container  */
+  img: { 
+    /* We imported that image with the dimensions of 520 x 480, one thing we can do, is everytime we do this, to keep the
+    ratio, to work with the property objectFit, in this case, we'll use the cover value,
+
+    cover: Makes the image keep the ratio and fit the container
+    
+    */
     objectFit: 'cover'
   },
 
   footer: {
+    /* This style will ensure that the footer be absolute to its parent, which is this container, and with its position
+    right at the bottom of the container
+    
+    the right and left of 0.25 rem, is just to make sure that the div takes all the width from the container, but with
+    0,25 rem on each "margin"
+    */
+
     position: 'absolute',
     bottom: '0.25rem',
     left: '0.25rem',
     right: '0.25rem',
     padding: '2rem',
+
+    
 
     borderRadius: 6,
 
@@ -41,6 +131,7 @@ export const Product = styled('div', {
     backgroundColor: 'rgba(0, 0, 0, 0.6)',
 
     transform: 'translateY(110%)',
+    /* So the text will be below the container height, so when we hover, it slides up*/
     opacity: 0,
     transition: 'all 0.2s ease-in-out',
 
@@ -57,6 +148,7 @@ export const Product = styled('div', {
   },
 
 
+    /* When we hover over our link, the footer will show up */
     '&:hover': {
       footer: {
         transform: 'translateY(0%)',
