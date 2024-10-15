0d84388faa90ecee2eacb842732b09e91c93c95d
Caio Ceretta <caioceretta@gmail.com>
2024-10-15 11:23:45 -0300

    feat: style success page

--- 05-ignite-shop/src/pages/success.tsx
+++ 05-ignite-shop/src/pages/success.tsx
@@ -1,11 +1,28 @@
-export interface SuccessProps {}
+import { ImageContainer } from "@/styles/pages/success"
+import { SuccessContainer } from "@/styles/pages/success"
+import Link from "next/link"
+
+export interface SuccessProps { }
 
 export const Success = (props: SuccessProps) => {
- return (
- <div className="justify-center items-center w-50"> 
-  <h1>Success</h1>
- </div> 
- ) 
+  return (
+    <SuccessContainer>
+      <h1>Purchase Completed</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Yay <strong>Caio Ceretta</strong>, your <strong>Camiseta Beyong the Limits</strong> it's already in transport to your house!
+      </p>
+
+      <Link href="/">
+        Return to the catalogue
+      </Link>
+
+    </SuccessContainer>
+  )
 }
 
 
