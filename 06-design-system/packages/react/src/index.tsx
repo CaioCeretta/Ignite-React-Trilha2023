788a125d9c173fe489ca862269be8de594acfff0
Caio Ceretta <caioceretta@gmail.com>
2024-10-17 20:56:46 -0300

    feat: stitches initial configuration

--- 06-design-system/packages/react/src/index.tsx
+++ 06-design-system/packages/react/src/index.tsx
@@ -1,6 +1,10 @@
-import { colors } from '@ignite-ui/tokens'
+import { styled } from './styles'
 
-console.log(colors)
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: ' ',
+})
 
 export function App() {
   return <h1 style={{ color: colors.ignite300 }}>Ciao mondo</h1>
