bfee2461918cfd585cd8293db12c459c09c1b60a
Caio Ceretta <caioceretta@gmail.com>
2024-10-21 14:36:58 -0300

    feat: heading docs

---
+++ 06-design-system/packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,22 @@
+
+import { Heading, HeadingProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Heading example'
+  }
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'h1 heading',
+    as: 'h1',
+    size: 'xl'
+  }
+}
+
