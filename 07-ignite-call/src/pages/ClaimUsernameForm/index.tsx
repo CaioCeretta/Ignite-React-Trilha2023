eeccd3a5155cbe2f88dc0e0a2db0c649a8a74763
Caio Ceretta <caioceretta@gmail.com>
2024-10-25 00:34:50 -0300

    feat: start hook form with zod

--- 07-ignite-call/src/pages/ClaimUsernameForm/index.tsx
+++ 07-ignite-call/src/pages/ClaimUsernameForm/index.tsx
@@ -1,16 +1,32 @@
+import { useForm } from 'react-hook-form'
+
 import { ArrowRight } from 'phosphor-react'
 
 import { Button, TextInput } from "@ignite-ui/react";
 import { Form } from "./style";
+import { z } from 'zod';
 
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormType>()
+
+  const claimUsernameFormSchema = z.object({
+    username: z.string()
+  })
+
+  type ClaimUsernameFormType = z.infer<typeof claimUsernameFormSchema>
+
+  async function handleClaimUsername(data: ClaimUsernameFormType) {
+    console.log(data.username)
+  }
+
   return (
-    <Form as="form">
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
       <TextInput
         size={'sm'}
         prefix="ignite.com/"
         placeholder="Seu Usuário"
+        {...register('username')}
       />
       <Button
         size={'sm'}
