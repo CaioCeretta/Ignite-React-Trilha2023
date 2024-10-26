8e77dd85c143d965bbad1b7dc23fcb8098cea341
Caio Ceretta <caioceretta@gmail.com>
2024-10-26 19:39:37 -0300

    feat: integratin user creation with prisma

--- 07-ignite-call/src/pages/register/index.page.tsx
+++ 07-ignite-call/src/pages/register/index.page.tsx
@@ -6,6 +6,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { api } from "../../lib/axios";
+import { AxiosError } from "axios";
 
 const registerFormSchema = z.object({
   username: z.string()
@@ -38,7 +40,22 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormType) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        fullname: data.fullname,
+        username: data.username
+      })
+
+      await router.push(`/register/connect-calendar`)
+    } catch (err) {
+      if(err instanceof AxiosError && err?.response?.data.message) {
+        alert(err.response.data.message)
+        return;
+      }
+
+      console.error(err)
+    
+    }
   }
 
   return (
