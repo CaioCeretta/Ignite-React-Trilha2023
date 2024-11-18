8b95b8e30f252fed9fd679392f906b974c7aa3f5
Caio Ceretta <caioceretta@gmail.com>
2024-11-18 09:34:59 -0300

    feat: add confirmation and cancel scheduling flow

--- 07-ignite-call/src/pages/schedule/[username]/ScheduleForm/index.page.tsx
+++ 07-ignite-call/src/pages/schedule/[username]/ScheduleForm/index.page.tsx
@@ -1,5 +1,24 @@
+'use client'
+
+import { useState } from 'react'
 import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from './ConfirmStep'
 
 export function ScheduleForm() {
-  return <CalendarStep />
+  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>()
+
+  if (selectedDateTime) {
+    return (
+      <ConfirmStep
+        onCancelConfirmation={handleClearSelectedDateTime}
+        schedulingDate={selectedDateTime}
+      />
+    )
+  }
+
+  function handleClearSelectedDateTime() {
+    setSelectedDateTime(null)
+  }
+
+  return <CalendarStep onSelectDateTime={setSelectedDateTime} />
 }
